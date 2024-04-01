8c2047321a0038d8f259a419ca5f10e1b7f43c94
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-04-01 21:30:56 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 // NAVIGATION
-export const NAV_LINKS = [
+export const  = [
   { href: "/", key: "home", label: "Home" },
   { href: "/", key: "how_hilink_work", label: "How Hilink Work?" },
   { href: "/", key: "services", label: "Services" },
